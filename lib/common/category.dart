940e5fd1912427e6251ff14a63e8073f397ff50c
Mohamed Sallad Abdule <149385253+moha-shiine@users.noreply.github.com>
2024-11-24 18:27:12 +0300

    dd

--- lib/common/category.dart
+++ lib/common/category.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_iconly/flutter_iconly.dart';
+import 'package:get/get_navigation/src/snackbar/snackbar.dart';
 import 'package:taskapp/color/them.dart';
 
 import '../view/model/category_model.dart';
@@ -31,49 +32,127 @@ class _categoryState extends State<category> {
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-      scrollDirection: Axis.horizontal,
-      child: Row(
-        children: taskList.asMap().entries.map((entry) {
-          int index = entry.key;
-          categoryMoldel task = entry.value;
-          return GestureDetector(
-            onTap: () => toggleSelection(index), // Toggle selection on tap
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 6),
-              child: Column(
-                children: [
-                  Container(
-                    height: 65,
-                    width: 65,
-                    decoration: BoxDecoration(
-                      color: task.isSelected
-                          ? Colors.white
-                          : Colorthem.PrimaryColor,
-                      borderRadius: BorderRadius.circular(16),
-                      border: Border.all(
-                        color: Colors.black,
-                      ), // Optional border
-                    ),
-                    child: Icon(
-                      task.icon,
-                      size: 30,
-                      color: task.isSelected ? Colors.green : Colors.white,
-                    ),
-                  ),
-                  SizedBox(height: 8),
-                  Text(
-                    task.label,
-                    style: TextStyle(
-                      fontSize: 14,
-                      fontWeight: FontWeight.bold,
-                      color: task.isSelected ? Colors.green : Colors.white,
-                    ),
-                  ),
-                ],
+        scrollDirection: Axis.horizontal,
+        child: Row(children: [
+          // int index = entry.key;
+          //  categoryMoldel task = entry.value;
+
+          //   onTap: () => toggleSelection(index), // Toggle selection on tap
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 6),
+            child: Row(
+              children: [
+                Headwiget(
+                  icon: IconlyLight.filter,
+                  title: "All Task",
+                  onTap: () {
+                    print('Widget tapped!');
+                  },
+                ),
+                Headwiget(
+                  icon: IconlyLight.filter,
+                  title: "All Task",
+                  onTap: () {},
+                ),
+                // Column(
+                //   children: [
+                //     Container(
+                //       height: 65,
+                //       width: 65,
+                //       decoration: BoxDecoration(
+                //         color: task.isSelected
+                //             ? Colors.white
+                //             : Colorthem.PrimaryColor,
+                //         borderRadius: BorderRadius.circular(16),
+                //         border: Border.all(
+                //           color: Colors.black,
+                //         ), // Optional border
+                //       ),
+                //       child: Icon(
+                //         task.icon,
+                //         size: 30,
+                //         color: task.isSelected ? Colors.green : Colors.white,
+                //       ),
+                //     ),
+                //     SizedBox(height: 8),
+                //     Text(
+                //       task.label,
+                //       style: TextStyle(
+                //         fontSize: 14,
+                //         fontWeight: FontWeight.bold,
+                //         color: task.isSelected ? Colors.green : Colors.white,
+                //       ),
+                //     ),
+                //   ],
+                // ),
+              ],
+            ),
+          ),
+        ]));
+  }
+}
+
+class Headwiget extends StatefulWidget {
+  final String title;
+  final VoidCallback onTap;
+
+  final IconData icon;
+  const Headwiget(
+      {super.key,
+      required this.title,
+      required this.icon,
+      required this.onTap});
+
+  @override
+  State<Headwiget> createState() => _HeadwigetState();
+}
+
+class _HeadwigetState extends State<Headwiget> {
+  @override
+  Widget build(BuildContext context) {
+    bool isSelected = false;
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      child: Column(
+        children: [
+          InkWell(
+            onTap: () {
+              setState(() {
+                isSelected = !isSelected; // Toggle selection state
+              });
+              widget.onTap();
+            },
+            child: Container(
+              height: 65,
+              width: 65,
+              decoration: BoxDecoration(
+                color:
+                    // ignore: dead_code
+                    isSelected ? Colorthem.thirtColor : Colorthem.PrimaryColor,
+                borderRadius: BorderRadius.circular(16),
+                border: Border.all(
+                  color: Colors.black,
+                ), // Optional border
+              ),
+              child: Icon(
+                widget.icon,
+                size: 30,
+                color:
+                    // ignore: dead_code
+                    isSelected ? Colorthem.PrimaryColor : Colorthem.thirtColor,
               ),
             ),
-          );
-        }).toList(),
+          ),
+          const SizedBox(height: 8),
+          Text(
+            widget.title,
+            style: const TextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+          ),
+        ],
       ),
     );
   }
